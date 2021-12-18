ced49bd9f369b111696ce76e68a7803b95996204
Olalekan Akintola <akinlekan28@yahoo.com>
2021-12-18 14:41:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
