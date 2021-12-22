0f1e15073c35c7d750239fe94d411a91869c63ac
Olalekan Akintola <akinlekan28@yahoo.com>
2021-12-22 13:25:47 +0100

    added wiston logger

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 //Route files
+const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 const app = express();
 
@@ -65,7 +67,27 @@ app.use(cors());
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Wiston request logger
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
+  })
+);
+
 // Mount routers
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
 
 app.use(errorHandler);
 
