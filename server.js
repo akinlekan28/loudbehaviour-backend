6daa8a1b7493a26b8c98c056ca9f8e02f7985983
Olalekan Akintola <akinlekan28@yahoo.com>
2021-12-22 13:15:21 +0100

    added routes folder

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require("cors");
 const errorHandler = require("./middleware/error");
+const winston = require("winston");
+const expressWinston = require("express-winston");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -16,3 +18,67 @@ dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
+
+//Route files
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+//wiston logger
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
+
+// Cookie parser
+app.use(cookieParser());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount routers
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
