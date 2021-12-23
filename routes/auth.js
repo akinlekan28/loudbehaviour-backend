6107a1bdf323081b3d14ed6f7343a0033b18b78d
Olalekan Akintola <akinlekan28@yahoo.com>
2021-12-23 09:02:11 +0100

    completed reset password

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login, forgotpassword } = require("../controllers/auth");
+const {
+  register,
+  login,
+  forgotpassword,
+  resetPassword,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgotpassword", forgotpassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
